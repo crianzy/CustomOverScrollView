4aae3e7d47a977cf6b7f2fb42ee528520dcc43d3
chenzhiyong <chen.zhiyong@zuimeia.biz>
2016-12-17 22:25:17 +0800

    first

---
+++ OverScrollView/app/src/main/java/czy/com/overscrollview/RecycleViewAdapter.java
@@ -0,0 +1,62 @@
+package czy.com.overscrollview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chenzhiyong on 16/1/12.
+ */
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder> {
+    private Context mContext;
+    private List<String> mStringList;
+
+    public RecycleViewAdapter(Context context) {
+        mContext = context;
+        mStringList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            mStringList.add("RecycleViewAdapter -- " + i);
+        }
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.recycle_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txt.setText(mStringList.get(position));
+        holder.txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mStringList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txt = (TextView) itemView.findViewById(R.id.txt);
+        }
+    }
+
+}
